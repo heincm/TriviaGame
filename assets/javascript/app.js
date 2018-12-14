146867b00ad0d32e16c871afe28a7ed372662179
Christopher Hein <chrishein@ChristophersMBP.gsu.edu>
2018-12-14 08:24:09 -0500

    adding pseudocode and test images

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+/* psuedo-coding 
+
+Create objects for trivia questions
+objects will need
+    question
+    array of answers
+    correct answer
+    A name?
+
+Will need to create two timer functions
+    Question timer
+        Will allow 15 seconds per question
+        Needs to be setInterval timer to display the amount of time left
+    Answer timer
+        Will be a setTimeout timer as it will fire only once
+        will allow 5-10 seconds to view answer before moving on
+
+Submit Function
+    Allow user to submit answer before time is up
+    will reset interval timer
+    will trigger 
+ 
+Grade function
+    will check for correct answer
+    if answer is correct, correct++
+    else wrong++
+
+Next Question function
+    after the answer is displayed, this will display all materials for the next question
+
+
+*/
+
+// Make object for game questions
+
+var quesBank = [{
+    name: "q1",
+    ques: "What was the original color of the hulk?",
+    ansBank: ["purple", "pink", "green", "grey"],
+    correct: "grey",
+},
+{
+    name: "q2",
+    ques: "What's the name of the prolific Marvel Comic genuis who passed away in 2018?",
+    ansBank: ["Stan Lee", "Your Mama", "My Mama", "Our Mama"],
+    correct: "Stan Lee",
+}];  // end questions object
