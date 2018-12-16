4f9fde6ec270b92ac6a00479a1ad40e2b21cb07a
Christopher Hein <chrishein@ChristophersMBP.attlocal.net>
2018-12-15 19:33:47 -0500

    Properly checks checked radio button for correct answer and updates correct and incorrect var

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,14 +16,8 @@ Will need to create two timer functions
         will allow 5-10 seconds to view answer before moving on
 
 Submit Function
-    Allow user to submit answer before time is up
     will reset interval timer
     will trigger 
- 
-Grade function
-    will check for correct answer
-    if answer is correct, correct++
-    else wrong++
 
 Next Question function
     after the answer is displayed, this will display all materials for the next question
@@ -32,37 +26,49 @@ Will use radio buttons for this as only one radio button can be selected at a ti
 
 */
 
+// variables that will keep track of correct and incorrect answers
+var correct = 0;
+var incorrect = 0;
+
 // Make object for game questions
 var quesBank = [{
     name: "q1", //this will be used as the name for the radio button
     ques: "What was the original color of the hulk?",
     ansBank: ["purple", "pink", "green", "grey"],
-    correct: "grey", //the correct answer for the question
+    answer: "grey", //the correct answer for the question
 },
 {
     name: "q2",
     ques: "What's the name of the prolific Marvel Comic genuis who passed away in 2018?",
     ansBank: ["Stan Lee", "Your Mama", "My Mama", "Our Mama"],
-    correct: "Stan Lee",
+    answer: "Stan Lee",
 }];  // end questions object
 
 //Creating the questions on the page
 for (var i = 0; i < quesBank[0].ansBank.length; i++) {
+    
     //create a button for each possible answer
     var button = $("<input>");
 
     //give each button a class
     button.addClass("buttons");
-    
+
     // add attributes to each button
     button.attr("type", "radio");
     button.attr("name", quesBank[0].name);
     button.attr("value", quesBank[0].ansBank[i]);
 
-    // add text for each button
-    
-
-    // add the buttons to the page
+    // add the buttons to the page and add text to each button
     $(".form").prepend(button, quesBank[0].ansBank[i]);
-    
 }
+
+// setting the click function to check answers
+$(".btn").on("click", function () {
+    var userResponse = $("input:checked").val();
+
+    if (userResponse === quesBank[0].answer) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+})
